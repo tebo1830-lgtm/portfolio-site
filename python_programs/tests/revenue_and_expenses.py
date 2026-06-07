6c78dfb4a2a0c429a9b5dad08fe69bc3372b655e
Sifiso Mokhele <smokhele@byupathway.edu>
2026-06-07 14:51:46 +0200

    restore

--- python_programs/tests/revenue_and_expenses.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from common_setup import run_test
-
-def test_revenue_and_expenses():
-    run_test("revenue_and_expenses", "Correct Revenue, Expenses, and Discretionary Calculations are Correct", "Error: Please make sure that your revenue, expenses and discretionary calculations are correct and outputted correctly")
-
-if __name__ == '__main__':
-    pytest.main()
